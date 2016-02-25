3de4c3566ec70da53c93ceea418a72ed4124c6bb
Stefan Dierauf <sdierauf@gmail.com>
2016-02-25 11:13:59 +0100

    checkpoint

--- src/main/scala/slacc/ast/Parser.scala
+++ src/main/scala/slacc/ast/Parser.scala
@@ -40,8 +40,108 @@ object Parser extends Pipeline[Iterator[Token], Program] {
     }
 
     def parseGoal: Program = {
+
+    }
+
+    def parseClassDecl: ClassDecl = {
+      var vars: List[VarDecl] = List()
+      var methods: List[MethodDecl] = List()
+      eat(CLASS)
+      var id: Identifier = parseIdentifier
+      var parent: Option[Identifier] = null
+
+      // Check if class has parent
+      currentToken.kind match {
+        case LESSTHAN => {
+          // has parent
+          eat(LESSTHAN)
+          eat(COLON)
+          parent = Option(parseIdentifier)
+        }
+        case _ => parent = Option(null)
+      }
+
+      eat(LBRACE)
+      // Build up vars
+      while (isFirstOfVarDecl) {
+        vars = parseVarDecl :: vars
+      }
+      // Build up methods
+      while (isFirstOfMethodDecl) {
+        methods = parseMethodDecl :: methods
+      }
+      eat(RBRACE)
+      new ClassDecl(id, parent, vars, methods)
+    }
+
+    def parseMainMethod: MainMethod = {
       ???
     }
+
+    def parseIdentifier: Identifier = {
+      currentToken.kind match {
+        case IDKIND => new Identifier(currentToken.value) // how the fuck get the string?
+        case _ => expected(IDKIND)
+      }
+    }
+
+    def parseVarDecl: VarDecl = {
+      eat(VAR)
+      val tpe: TypeTree = parseType
+      eat(COLON)
+      val id: Identifier = parseIdentifier
+      eat(SEMICOLON)
+      new VarDecl(tpe, id)
+    }
+
+    def parseMethodDecl: MethodDecl = {
+      eat(METHOD)
+      val id: Identifier = parseIdentifier
+
+      eat(LPAREN)
+      var args: List[Formal] = List()
+      // build up args (which is a list of 'Formal's)
+      if (isNextOfFormal) {
+        args = parseFormal :: args
+        while (isComma) {
+          eat(COMMA)
+          args = parseFormal :: args
+        }
+      }
+      eat(RPAREN)
+
+      // parse return type
+      eat(COLON)
+      val retType: TypeTree = parseType
+
+      eat(EQUALS)
+      eat(RBRACE)
+
+      // parse var declarations
+      var vars: List[VarDecl] = List()
+      while (isFirstOfVarDecl) {
+        vars = parseVarDecl :: vars
+      }
+
+      // parse Expressions
+      var exprs: List[ExprTree] = List()
+      if (isFirstOfExpression) {
+        exprs = 
+      }
+
+      new MethodDecl(retType, id, args, vars, exprs, retExpr)
+    }
+
+    def parseType: TypeTree = {
+      ???
+    }
+
+    def parseFormal: Formal = {
+      ???
+    }
+
+
+
 
     readToken
     val tree = parseGoal
