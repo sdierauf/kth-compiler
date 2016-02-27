d899ab6962729d0de658343608bff8f6309f766f
sierraa <sielynanderson@gmail.com>
2016-02-27 11:19:56 +0100

    finishing some print stubs

--- src/main/scala/slacc/ast/Printer.scala
+++ src/main/scala/slacc/ast/Printer.scala
@@ -77,111 +77,122 @@ object Printer {
   }
 
   def printAnd(t: And): String = {
-    ???
+    apply(t.lhs) + " && " + apply(t.rhs)
   }
 
   def printOr(t: Or): String ={
-    ???
+    apply(t.lhs) + " || " + apply(t.rhs)
   }
 
   def printPlus(t: Plus): String = {
-    ???
+    apply(t.lhs) + " + " + apply(t.rhs)
   }
 
   def printMinus(t: Minus): String ={
-    ???
+    apply(t.lhs) + " - " + apply(t.rhs)
   }
 
   def printTimes(t: Times): String ={
-    ???
+    apply(t.lhs) + " * " + apply(t.rhs)
   }
 
   def printDiv(t: Div): String = {
-    ???
+    apply(t.lhs) + " / " + apply(t.rhs)
   }
 
   def printLessThan(t: LessThan): String = {
-    ???
+    apply(t.lhs) + " < " + apply(t.rhs)
   }
 
   def printEquals(t: Equals): String ={
-    ???
+    apply(t.lhs) + " == " + apply(t.rhs)
   }
 
   def printArrayRead(t: ArrayRead): String ={
-    ???
+    apply(t.arr) + "[" + apply(t.index) + "]" 
   }
 
   def printArrayLength(t: ArrayLength): String ={
-    ???
+    apply(t.arr) + ".length"
   }
 
   def printMethodCall(t: MethodCall): String = {
-    ???
+    var s = new StringBuilder(apply(t.obj) + " " + apply(t.meth) +"(")
+    val arg : List[String] = t.args.map(e => apply(e))
+    s ++= expressions.mkString(",")  
+    (s ++= ")").toString
   }
 
   def printIntLit(t: IntLit): String ={
-    ???
+    t.value.toString
   }
 
   def printStringLit(t: StringLit): String ={
-    ???
+    t.value
   }
 
   def printTrue(t: True): String = {
-    ???
+    "true"
   }
 
   def printFalse(t: False): String = {
-    ???
+    "false"
   }
 
   def printIdentifier(t: Identifier): String = {
-    ???
+    t.value
   }
 
   def printSelf(t: Self): String = {
-    ???
+    "self"
   }
 
   def printNewIntArray(t: NewIntArray): String = {
-    ???
+    "new " + "Int[" + apply(t.size) "]"
   }
 
   def printNew(t: New): String = {
-    ???
+    "new " + apply(t.tpe)
   }
 
   def printNot(t: Not): String = {
-    ???
+    "!" + apply(t.expr)
   }
 
   def printBlock(t: Block): String = {
-    ???
+    var s = new StringBuilder("{")
+    val expressions : List[String] = t.exprs.map(e => apply(e))
+    s ++= expressions.mkString(";\n") // thank you brilliant stefan 
+    (s ++= "}").toString
   }
 
-  def printIf(t: If): String = {
-    ???
+  def printIf(t: If): String = t.els match {
+    case Some(els) => {
+      elseBody = "else " + printExpression(ex)
+    } case None => {
+      elseBody = ""
+    }
+    "if (" + apply(t.expr) + ")" + apply(thn) + elseBody
   }
 
   def printWhile(t: While): String = {
-    ???
+    "while (" + apply(t.cond) + ") " + apply(t.body)
   }
 
   def printPrintln(t: Println): String = {
-    ???
+    "println(" + apply(t.expr) +")"
   }
 
   def printAssign(t: Assign): String = {
-    ???
+    apply(t.id) + " = " + apply(t.expr)
   }
 
   def printArrayAssign(t: ArrayAssign): String = {
-    ???
+    apply(t.id) + "[" + apply(t.index) + "] =" + apply(t.expr) 
   }
 
   def printStrof(t: Strof): String = {
-    ???
+     "srtOf(" + apply(t.expr) + ")"
   }
 
 }
