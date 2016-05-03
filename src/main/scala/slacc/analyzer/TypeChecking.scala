a2ca6c17e0f33fe847b257889c1d74dfae4b2ead
Stefan Dierauf <sdierauf@gmail.com>
2016-05-03 15:52:23 +0200

    stubs stubs stubs stubs

--- src/main/scala/slacc/analyzer/TypeChecking.scala
+++ src/main/scala/slacc/analyzer/TypeChecking.scala
@@ -12,143 +12,142 @@ object TypeChecking extends Pipeline[Program, Program] {
     import ctx.reporter._
 
     def tcExpr(expr: ExprTree, expected: Type*): Type = {
-      val tpe: Type = ??? // TODO: Compute type for each kind of expression
-      expr match {
-        case e: And =>
-        case e: Or =>
-        case e: Plus =>
-        case e: Minus =>
-        case e: Times =>
-        case e: Div =>
-        case e: LessThan =>
-        case e: Equals =>
-        case e: ArrayRead =>
-        case e: ArrayLength =>
-        case e: MethodCall =>
-        case e: IntLit =>
-        case e: StringLit =>
-        case e: True =>
-        case e: False =>
-        case e: Identifier =>
-        case e: Self =>
-        case e: NewIntArray =>
-        case e: New =>
-        case e: Not =>
-        case e: Block =>
-        case e: If =>
-        case e: While =>
-        case e: Println =>
-        case e: Assign =>
-        case e: ArrayAssign =>
-        case e: Strof =>
+      val tpe: Type = expr match {
+        case e: And => tcAnd(e)
+        case e: Or => tcOr(e)
+        case e: Plus => tcPlus(e)
+        case e: Minus => tcMinus(e)
+        case e: Times => tcTimes(e)
+        case e: Div => tcDiv(e)
+        case e: LessThan => tcLessThan(e)
+        case e: Equals => tcEquals(e)
+        case e: ArrayRead => tcArrayRead(e)
+        case e: ArrayLength => tcArrayLength(e)
+        case e: MethodCall => tcMethodCall(e)
+        case e: IntLit => tcIntLit(e)
+        case e: StringLit => tcStringLit(e)
+        case e: True => tcTrue(e)
+        case e: False => tcFalse(e)
+        case e: Identifier => tcIdentifier(e)
+        case e: Self => tcSelf(e)
+        case e: NewIntArray => tcNewIntArray(e)
+        case e: New => tcNew(e)
+        case e: Not => tcNot(e)
+        case e: Block => tcBlock(e)
+        case e: If => tcIf(e)
+        case e: While => tcWhile(e)
+        case e: Println => tcPrintln(e)
+        case e: Assign => tcAssign(e)
+        case e: ArrayAssign => tcArrayAssign(e)
+        case e: Strof => tcStrof(e)
         case _ => fatal ("tcExpr: what happened")
       }
 
-      def tcAnd(e: And): Unit = {
+      def tcAnd(e: And): Type = {
 
       }
 
-      def tcOr(e: Or): Unit = {
+      def tcOr(e: Or): Type = {
 
       }
 
-      def tcPlus(e: Plus): Unit = {
+      def tcPlus(e: Plus): Type = {
 
       }
 
-      def tcMinus(e: Minus): Unit = {
+      def tcMinus(e: Minus): Type = {
 
       }
 
-      def tcTimes(e: Times): Unit = {
+      def tcTimes(e: Times): Type = {
 
       }
 
-      def tcDiv(e: Div): Unit = {
+      def tcDiv(e: Div): Type = {
 
       }
 
-      def tcLessThan(e: LessThan): Unit = {
+      def tcLessThan(e: LessThan): Type = {
 
       }
 
-      def tcEquals(e: Equals): Unit = {
+      def tcEquals(e: Equals): Type = {
 
       }
 
-      def tcArrayRead(e: ArrayRead): Unit = {
+      def tcArrayRead(e: ArrayRead): Type = {
 
       }
 
-      def tcArrayLength(e: ArrayLength): Unit = {
+      def tcArrayLength(e: ArrayLength): Type = {
 
       }
 
-      def tcMethodCall(e: MethodCall): Unit = {
+      def tcMethodCall(e: MethodCall): Type = {
 
       }
 
-      def tcIntLit(e: IntLit): Unit = {
+      def tcIntLit(e: IntLit): Type = {
 
       }
 
-      def tcStringLit(e: StringLit): Unit = {
+      def tcStringLit(e: StringLit): Type = {
 
       }
 
-      def tcTrue(e: True): Unit = {
+      def tcTrue(e: True): Type = {
 
       }
 
-      def tcFalse(e: False): Unit = {
+      def tcFalse(e: False): Type = {
 
       }
 
-      def tcIdentifier(e: Identifier): Unit = {
+      def tcIdentifier(e: Identifier): Type = {
 
       }
 
-      def tcSelf(e: Self): Unit = {
+      def tcSelf(e: Self): Type = {
 
       }
 
-      def tcNewIntArray(e: NewIntArray): Unit = {
+      def tcNewIntArray(e: NewIntArray): Type = {
 
       }
 
-      def tcNew(e: New): Unit = {
+      def tcNew(e: New): Type = {
 
       }
 
-      def tcNot(e: Not): Unit = {
+      def tcNot(e: Not): Type = {
 
       }
 
-      def tcBlock(e: Block): Unit = {
+      def tcBlock(e: Block): Type = {
 
       }
 
-      def tcIf(e: If): Unit = {
+      def tcIf(e: If): Type = {
 
       }
 
-      def tcWhile(e: While): Unit = {
+      def tcWhile(e: While): Type = {
 
       }
 
-      def tcPrintln(e: Println): Unit = {
+      def tcPrintln(e: Println): Type = {
 
       }
 
-      def tcAssign(e: Assign): Unit = {
+      def tcAssign(e: Assign): Type = {
 
       }
 
-      def tcArrayAssign(e: ArrayAssign): Unit = {
+      def tcArrayAssign(e: ArrayAssign): Type = {
 
       }
 
-      def tcStrof(e: Strof): Unit = {
+      def tcStrof(e: Strof): Type = {
 
       }
 
