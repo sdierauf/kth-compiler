af3f70d99321e4becbcdb41f8cdeb41544fc8cb5
Stefan Dierauf <sdierauf@gmail.com>
2016-05-06 11:54:30 +0200

    most obtuse error ever ty scala

--- src/main/scala/slacc/analyzer/TypeChecking.scala
+++ src/main/scala/slacc/analyzer/TypeChecking.scala
@@ -13,7 +13,7 @@ object TypeChecking extends Pipeline[Program, Program] {
     import ctx.reporter._
 
     def tcExpr(expr: ExprTree, expected: Type*): Type = {
-      val tpe: Type = expr match {
+      val tpe = expr match {
         case e: And => tcAnd(e)
         case e: Or => tcOr(e)
         case e: Plus => tcPlus(e)
@@ -43,179 +43,7 @@ object TypeChecking extends Pipeline[Program, Program] {
         case e: Strof => tcStrof(e)
         case _ => fatal ("tcExpr: what happened")
       }
-
-      def tcAnd(e: And): Type = {
-        tcExpr(e.lhs, TBoolean)
-        tcExpr(e.rhs, TBoolean)
-        TBoolean
-      }
-
-      def tcOr(e: Or): Type = {
-        tcExpr(e.lhs, TBoolean)
-        tcExpr(e.rhs, TBoolean)
-        TBoolean
-      }
-
-      def tcPlus(e: Plus): Type = {
-        val possibleTypes = TInt :: TString :: List()
-        val l = tcExpr(e.lhs, possibleTypes:_*)
-        val r = tcExpr(e.rhs, possibleTypes:_*)
-        l match {
-          case TInt => r match {
-            case TInt => TInt
-            case TString => TString
-            case _ => fatal("tcPlus: werent strings or ints")
-          }
-          case TString => r match {
-            case TInt => TString
-            case TString => TString
-            case _ => fatal("tcPlus: werent strings or ints")
-          }
-          case _ => fatal("tcPlus: werent strings or ints")
-        }
-      }
-
-      def tcMinus(e: Minus): Type = {
-        val l = tcExpr(e.lhs, TInt)
-        val r = tcExpr(e.rhs, TInt)
-        TInt
-      }
-
-      def tcTimes(e: Times): Type = {
-        val l = tcExpr(e.lhs, TInt)
-        val r = tcExpr(e.rhs, TInt)
-        TInt
-      }
-
-      def tcDiv(e: Div): Type = {
-        val l = tcExpr(e.lhs, TInt)
-        val r = tcExpr(e.rhs, TInt)
-        TInt
-      }
-
-      def tcLessThan(e: LessThan): Type = {
-        val l = tcExpr(e.lhs, TInt)
-        val r = tcExpr(e.rhs, TInt)
-        TInt
-      }
-
-      def tcEquals(e: Equals): Type = {
-        val primitives = TInt :: TString :: TBoolean :: TIntArray :: List()
-        val accepted: List[Type] = anyObject :: primitives
-        val l = tcExpr(e.lhs, accepted:_*)
-        l match {
-          case TObject(klass) => tcExpr(e.rhs, anyObject); TBoolean
-          case p if primitives.contains(l) => tcExpr(e.rhs, p); TBoolean
-          case _ => {
-            val r : Type = tcExpr(e.rhs)
-            fatal("Type error: Equality operator requires compatible types, but found types: "
-              + l + " and " + r)
-            TError
-          }
-        }
-      }
-
-      def tcArrayRead(e: ArrayRead): Type = {
-        tcExpr(e.arr, TIntArray)
-        tcExpr(e.index, TInt)
-      }
-
-      def tcArrayLength(e: ArrayLength): Type = {
-        tcExpr(e.arr, TIntArray)
-        TInt
-      }
-
-      def tcMethodCall(e: MethodCall): Type = {
-        tcExpr(e.obj, anyObject)
-        val params = e.meth.getSymbol
-        var argListTypes = List[Type]()
-        params match {
-          case p: MethodSymbol => {
-            p.argList.foreach(a => argListTypes:+a.getType)
-          }
-          case _ => TError // should never happen
-        }
-        if (e.args.length != argListTypes.length) TError // is this okay
-        for ((arg, t) <- (e.args zip argListTypes)) yield tcExpr(arg, t)
-        tcExpr(e.meth, e.meth.getType)
-      }
-
-      def tcIntLit(e: IntLit): Type = TInt
-
-      def tcStringLit(e: StringLit): Type = TString
-
-      def tcTrue(e: True): Type = TBoolean
-
-      def tcFalse(e: False): Type = TBoolean
-
-      def tcIdentifier(e: Identifier): Type = {
-        e.getType
-      }
-
-      def tcSelf(e: Self): Type = {
-        e.getSymbol.getType
-      }
-
-      def tcNewIntArray(e: NewIntArray): Type = {
-        tcExpr(e.size, TInt)
-      }
-
-      def tcNew(e: New): Type = {
-        tcExpr(e.tpe)
-      }
-
-      def tcNot(e: Not): Type = {
-        tcExpr(e.expr, TBoolean)
-      }
-
-      def tcBlock(e: Block): Type = {
-        e.exprs.foreach(ex => tcExpr(ex))
-        TUnit // is this the type of a block
-      }
-
-      def tcIf(e: If): Type = {
-        tcExpr(e.expr, TBoolean)
-        val thnBranch = tcExpr(e.thn)
-        e.els match {
-          case Some(els) => {
-            val elsBranch = tcExpr(els)
-            if (thnBranch == elsBranch) thnBranch
-            else TError
-          } case None => thnBranch
-        }
-      }
-
-      def tcWhile(e: While): Type = {
-        tcExpr(e.cond, TBoolean)
-        tcExpr(e.body, TUnit)
-        TUnit
-      }
-
-      def tcPrintln(e: Println): Type = {
-        tcExpr(e.expr, TString)
-        TUnit
-      }
-
-      def tcAssign(e: Assign): Type = {
-        val lhs = tcExpr(e.id)
-        val rhs = tcExpr(e.expr)
-        if (lhs == rhs) TUnit // ya
-        else if (rhs.isSubTypeOf(lhs)) TUnit // this is ok
-        else TError
-      }
-
-      def tcArrayAssign(e: ArrayAssign): Type = {
-         tcExpr(e.id, TIntArray)
-         tcExpr(e.index, TInt)
-         tcExpr(e.expr, TInt)
-         TUnit
-      }
-
-      def tcStrof(e: Strof): Type = {
-         tcExpr(e.expr, TInt, TBoolean)
-         TString
-      }
-
+      
       // Check result and return a valid type in case of error
       if (expected.isEmpty) {
         tpe
@@ -225,6 +53,178 @@ object TypeChecking extends Pipeline[Program, Program] {
       } else {
         tpe
       }
+    }
+
+    def tcAnd(e: And): Type = {
+      tcExpr(e.lhs, TBoolean)
+      tcExpr(e.rhs, TBoolean)
+      TBoolean
+    }
+
+    def tcOr(e: Or): Type = {
+      tcExpr(e.lhs, TBoolean)
+      tcExpr(e.rhs, TBoolean)
+      TBoolean
+    }
+
+    def tcPlus(e: Plus): Type = {
+      val possibleTypes = TInt :: TString :: List()
+      val l = tcExpr(e.lhs, possibleTypes:_*)
+      val r = tcExpr(e.rhs, possibleTypes:_*)
+      l match {
+        case TInt => r match {
+          case TInt => TInt
+          case TString => TString
+          case _ => fatal("tcPlus: werent strings or ints")
+        }
+        case TString => r match {
+          case TInt => TString
+          case TString => TString
+          case _ => fatal("tcPlus: werent strings or ints")
+        }
+        case _ => fatal("tcPlus: werent strings or ints")
+      }
+    }
+
+    def tcMinus(e: Minus): Type = {
+      val l = tcExpr(e.lhs, TInt)
+      val r = tcExpr(e.rhs, TInt)
+      TInt
+    }
+
+    def tcTimes(e: Times): Type = {
+      val l = tcExpr(e.lhs, TInt)
+      val r = tcExpr(e.rhs, TInt)
+      TInt
+    }
+
+    def tcDiv(e: Div): Type = {
+      val l = tcExpr(e.lhs, TInt)
+      val r = tcExpr(e.rhs, TInt)
+      TInt
+    }
+
+    def tcLessThan(e: LessThan): Type = {
+      val l = tcExpr(e.lhs, TInt)
+      val r = tcExpr(e.rhs, TInt)
+      TInt
+    }
+
+    def tcEquals(e: Equals): Type = {
+      val primitives = TInt :: TString :: TBoolean :: TIntArray :: List()
+      val accepted: List[Type] = anyObject :: primitives
+      val l = tcExpr(e.lhs, accepted:_*)
+      l match {
+        case TObject(klass) => tcExpr(e.rhs, anyObject); TBoolean
+        case p if primitives.contains(l) => tcExpr(e.rhs, p); TBoolean
+        case _ => {
+          val r : Type = tcExpr(e.rhs)
+          fatal("Type error: Equality operator requires compatible types, but found types: "
+            + l + " and " + r)
+          TError
+        }
+      }
+    }
+
+    def tcArrayRead(e: ArrayRead): Type = {
+      tcExpr(e.arr, TIntArray)
+      tcExpr(e.index, TInt)
+    }
+
+    def tcArrayLength(e: ArrayLength): Type = {
+      tcExpr(e.arr, TIntArray)
+      TInt
+    }
+
+    def tcMethodCall(e: MethodCall): Type = {
+      tcExpr(e.obj, anyObject)
+      val params = e.meth.getSymbol
+      var argListTypes = List[Type]()
+      params match {
+        case p: MethodSymbol => {
+          p.argList.foreach(a => argListTypes:+a.getType)
+        }
+        case _ => TError // should never happen
+      }
+      if (e.args.length != argListTypes.length) TError // is this okay
+      for ((arg, t) <- (e.args zip argListTypes)) yield tcExpr(arg, t)
+      tcExpr(e.meth, e.meth.getType)
+    }
+
+    def tcIntLit(e: IntLit): Type = TInt
+
+    def tcStringLit(e: StringLit): Type = TString
+
+    def tcTrue(e: True): Type = TBoolean
+
+    def tcFalse(e: False): Type = TBoolean
+
+    def tcIdentifier(e: Identifier): Type = {
+      e.getType
+    }
+
+    def tcSelf(e: Self): Type = {
+      e.getSymbol.getType
+    }
+
+    def tcNewIntArray(e: NewIntArray): Type = {
+      tcExpr(e.size, TInt)
+    }
+
+    def tcNew(e: New): Type = {
+      tcExpr(e.tpe)
+    }
+
+    def tcNot(e: Not): Type = {
+      tcExpr(e.expr, TBoolean)
+    }
+
+    def tcBlock(e: Block): Type = {
+      e.exprs.foreach(ex => tcExpr(ex))
+      TUnit // is this the type of a block
+    }
+
+    def tcIf(e: If): Type = {
+      tcExpr(e.expr, TBoolean)
+      val thnBranch = tcExpr(e.thn)
+      e.els match {
+        case Some(els) => {
+          val elsBranch = tcExpr(els)
+          if (thnBranch == elsBranch) thnBranch
+          else TError
+        } case None => thnBranch
+      }
+    }
+
+    def tcWhile(e: While): Type = {
+      tcExpr(e.cond, TBoolean)
+      tcExpr(e.body, TUnit)
+      TUnit
+    }
+
+    def tcPrintln(e: Println): Type = {
+      tcExpr(e.expr, TString)
+      TUnit
+    }
+
+    def tcAssign(e: Assign): Type = {
+      val lhs = tcExpr(e.id)
+      val rhs = tcExpr(e.expr)
+      if (lhs == rhs) TUnit // ya
+      else if (rhs.isSubTypeOf(lhs)) TUnit // this is ok
+      else TError
+    }
+
+    def tcArrayAssign(e: ArrayAssign): Type = {
+      tcExpr(e.id, TIntArray)
+      tcExpr(e.index, TInt)
+      tcExpr(e.expr, TInt)
+      TUnit
+    }
+
+    def tcStrof(e: Strof): Type = {
+      tcExpr(e.expr, TInt, TBoolean)
+      TString
     }
 
     prog
